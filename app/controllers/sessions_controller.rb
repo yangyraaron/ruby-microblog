c2f93136fd7998cddf6b6904cccaca2ae4ac20c8
aaron <yangyr.aaron@gmail.com>
2013-05-26 16:24:22 +0800

    mofidy home page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
-	skip_before_filter :authorize
+	skip_before_filter :authorize,:only=>[:new,:create]
+  layout "content",:only=>[:index]
   def index
   	respond_to do |format|
   		logger.info("the format is #{format}")
